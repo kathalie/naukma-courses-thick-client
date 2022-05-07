8107f8f207bf151841db3fb6ec4d64b233daa1d0
Oleg <noneedinmagic@gmail.com>
2022-05-08 01:59:58 +0300

    Add TypeORM

---
+++ ormconfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  type: 'mysql',
+  host: '127.0.0.1',
+  port: 33080,
+  username: 'root',
+  password: 'root',
+  database: 'edu_backend_courses',
+  entities: ['dist/**/*.entity{.ts,.js}'],
+};
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CourseModule } from './modules/course/course.module';
 import { ScheduleModule } from './modules/schedule/schedule.module';
 import { SystemModule } from './modules/system/system.module';
 
 @Module({
-  imports: [CourseModule, ScheduleModule, SystemModule],
+  imports: [CourseModule, ScheduleModule, SystemModule, TypeOrmModule.forRoot()],
   controllers: [],
   providers: [],
 })
